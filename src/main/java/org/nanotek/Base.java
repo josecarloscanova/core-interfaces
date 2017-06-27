f3eceaa91545fa5f27b4c5e7d79b3394747b5064
Darth Vader <jose.carlos.canova@gmail.com>
2017-06-27 13:28:14 -0300

    initial commit

---
+++ src/main/java/org/nanotek/Base.java
@@ -0,0 +1,26 @@
+package org.nanotek;
+
+import java.io.Serializable;
+import java.util.Optional;
+import java.util.function.Supplier;
+
+public interface Base<K extends Serializable> extends Identifiable <K>  {
+
+	void setId(K id);
+	
+	default <T> T newAnyType(Supplier<T> supplier)
+	{ 
+		return supplier.get();
+	}
+	
+	default <T> T ofNullable(T dest , Supplier<T> supplier) 
+	{ 
+		return Optional.ofNullable(dest).orElseGet(supplier); 
+	}
+	
+	default <T extends Base<?>> T newType(Supplier<T> baseSupplier)
+	{ 
+		return baseSupplier.get();
+	}
+	
+}
